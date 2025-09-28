2f0600e828913233d7a44577e9d11289049d9f52
Daniel Fernández Jaime <danielfernandddez@gmail.com>
2025-09-28 16:46:54 +0200

    CookieYes - Allow YT videos

--- frontend/src/components/VideoSection.jsx
+++ frontend/src/components/VideoSection.jsx
@@ -15,6 +15,7 @@ const VideoSection = () => {
                     frameBorder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                     allowFullScreen
+                    data-cookieyes="ignore"
                 ></iframe>
             </div>
         </section>
