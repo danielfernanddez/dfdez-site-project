ca2001cc8d599b767bfd5761dea64b46a3c177a0
Daniel Fernández Jaime <danielfernandddez@gmail.com>
2025-10-04 19:50:06 +0200

    Fix Video iFrame

--- frontend/src/components/VideoSection.jsx
+++ frontend/src/components/VideoSection.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import LiteYouTubeEmbed from 'react-lite-youtube-embed';
-import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import './VideoSection.css';
 
 const VideoSection = () => {
@@ -8,15 +6,20 @@ const VideoSection = () => {
         <section className="VideoSection">
             <h2 className="VideoSection-title">Unique Team - Hip Hop International Championship</h2>
             <div className="VideoSection-wrapper">
-                <LiteYouTubeEmbed 
-                    id="iAyDF1vj604"
-                    title="Unique - Spain | MegaCrew Division | 2025 World Hip Hop Dance Championship Semifinals"
-                    params='enablejsapi=1'
-                />
+                <iframe 
+                    width="560" 
+                    height="315" 
+                    src="https://www.youtube.com/embed/iAyDF1vj604?enablejsapi=1" 
+                    title="Unique - Spain | MegaCrew Division | 2023 World Hip Hop Dance Championship Semis" 
+                    frameBorder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowFullScreen
+                ></iframe>
             </div>
         </section>
     );
 };
 
 export default VideoSection;
+
 
